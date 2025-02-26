844e464dd3a865939221fa4bdf6a7e3142bb0543
kuuchan-code <kuuchanxn@gmail.com>
2025-02-27 01:51:40 +0900

    worker名を変更

--- src/app/sendNotification.js
+++ src/app/sendNotification.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 
 export async function sendNotification(task) {
   // task は { fcmToken, title, priority } などのプロパティを持つと仮定
-  const response = await fetch("https://my-tasks-app.kuuchanxn.workers.dev/", {
+  const response = await fetch("https://my-tasks-worker.kuuchanxn.workers.dev /", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
