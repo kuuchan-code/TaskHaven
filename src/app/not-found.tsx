1a83abf778f7a1b20ffcdf8d11df77d9af6304ae
kuuchan-code <kuuchanxn@gmail.com>
2025-03-02 20:24:56 +0900

    update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-      <p className="mt-4">Sorry, we couldn't find the page you were looking for.</p>
+      <p className="mt-4">Sorry, we couldn&apos;t find the page you were looking for.</p>
     </div>
   );
 }
